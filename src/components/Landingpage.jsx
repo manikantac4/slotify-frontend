47cebce41f0ae943eb0a18a643e57d3686364c98
Panduranga <panduranga1797@gmail.com>
2026-04-18 15:41:00 +0530

    components attach

--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -2,12 +2,14 @@ import Hero from "../components/hero";
 import React from "react";
 import About from "../components/about";
 import Features from "../components/features";
+import Footer from "../components/footer";
 export default function Landing() {
   return (
     <div>
       <Hero />
       <About />
       <Features />
+        <Footer />
     </div>
   );
 }
